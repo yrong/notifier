9d12c82fa30a36ed0ffeafce92865d646d37c493
yrong <yrong1997@gmail.com>
2017-07-24 12:46:18 +0800

    通知接口路由变更＆通知model变更＆配合scirichon-common包依赖整理(1.5h)
    1,通知接口路由变更
    2,配合scirichon-common包依赖整理
    3,model变更（user变更为object字段)

--- models/notification.js
+++ models/notification.js
@@ -2,14 +2,13 @@ module.exports = function (sequelize, DataTypes) {
     var Nofification = sequelize.define("Notification",
         {
             uuid: {type: DataTypes.UUID, allowNull: false, primaryKey: true,defaultValue: DataTypes.UUIDV4},
-            user_id:{type: DataTypes.INTEGER,allowNull: false},
+            user:{type: DataTypes.JSONB,allowNull: false},
             action:{type: DataTypes.STRING, allowNull: false},
             old:{type: DataTypes.JSONB},
             new:{type: DataTypes.JSONB},
             update:{type: DataTypes.JSONB},
             type:{type:DataTypes.STRING,allowNull: false},
-            notified_user:{type:DataTypes.ARRAY(DataTypes.INTEGER),defaultValue:[-1]},
-            avatar:{type:DataTypes.STRING}
+            notified_user:{type:DataTypes.ARRAY(DataTypes.INTEGER),defaultValue:[-1]}
         });
     return Nofification;
 };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const _ = require('lodash')
 const Router = require('koa-router')
 const notifications = require('./notification')
 const router = new Router();
-router.use(`/notifications`,  notifications.routes(),notifications.allowedMethods())
+router.use(`/api/notifications`, notifications.routes(),notifications.allowedMethods())
 
 module.exports = router
