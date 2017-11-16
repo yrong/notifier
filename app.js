b7e6091a367c14af6e4ec3bd1a6ca8b8e75f3cdd
yrong <yrong1997@gmail.com>
2017-11-16 20:47:38 +0800

    notifier通过环境变量加载配置&配合容器化改造依赖升级(0.5h)

--- app.js
+++ app.js
@@ -16,18 +16,20 @@ const check_token = require('scirichon-token-checker')
 const acl_checker = require('scirichon-acl-checker')
 const models = require('./models')
 const router = require('./routes')
+const schema = require('redis-json-schema')
 
 
 /**
  * init middlewares
  */
-
+const redisOption = {host:`${process.env['REDIS_HOST']||config.get('redis.host')}`,port:config.get('redis.port')}
+const cache_loadUrl = {cmdb_url:`http://${config.get('privateIP') || 'localhost'}:${config.get('cmdb.port')}/api`}
 const app = new Koa();
 app.use(cors({ credentials: true }))
 app.use(bodyParser())
 app.use(responseWrapper())
 app.use(check_token({check_token_url:`http://${config.get('privateIP')||'localhost'}:${config.get('auth.port')}/auth/check`}))
-app.use(acl_checker.middleware)
+app.use(acl_checker({redisOption}))
 
 /**
  * init routes
@@ -41,13 +43,14 @@ notification_io.attach(app)
 /**
  * start server
  */
-scirichon_cache.initialize({cmdb_url: `http://${config.get('privateIP') || 'localhost'}:${config.get('cmdb.port')}/api`}).then((schemas)=>{
+schema.loadSchemas({redisOption}).then((schemas)=>{
     if (schemas && schemas.length) {
+        scirichon_cache.initialize({loadUrl: cache_loadUrl,redisOption})
         app.listen(config.get('notifier.port'), () => {
-            console.log('app started')
+            logger.info('App started')
         })
     }else{
-        logger.fatal(`no schemas found,npm run init in cmdb first!`)
+        logger.fatal(`no schemas found,npm run init first!`)
         process.exit(-2)
     }
 })
