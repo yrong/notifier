fdc027cc9fed76af277b44bef6242f25c9e058c9
yrong <yrong1997@gmail.com>
2018-01-12 17:33:24 +0800

    通知引用对象映射重构&订单历史查询用例(3h)

--- helper/resultMapping.js
+++ helper/resultMapping.js
@@ -1,9 +1,7 @@
-const scirichon_cache = require('scirichon-cache')
 const _ = require('lodash')
 const common = require('scirichon-common')
 const config = require('config')
-const schema = require('redis-json-schema')
-const uuid_validator = require('uuid-validate')
+const scirichon_response_mapper = require('scirichon-response-mapper')
 
 const notificationMapping = async function(notification){
     notification.actor = notification.user
@@ -14,56 +12,10 @@ const notificationMapping = async function(notification){
     return notification//common.pruneEmpty(notification);
 }
 
-const referencedObjectMapper = async (val,props)=>{
-    if(props.properties){
-        for(let key in props.properties){
-            if(val[key]&&props.properties[key].schema){
-                if(uuid_validator(val[key])){
-                    val[key]= await scirichon_cache.getItemByCategoryAndID(props.properties[key].schema,val[key])||val[key]
-                }
-            }
-        }
-    }
-    return val
-}
-
-const resultMapperBySchema = async (val) => {
-    let properties = schema.getSchemaProperties(val.category),objs,obj
-    for (let key in val) {
-        if (val[key] && properties[key]) {
-            if(properties[key].schema){
-                val[key] = await scirichon_cache.getItemByCategoryAndID(properties[key].schema,val[key]) || val[key]
-            }
-            else if(val[key].length&&properties[key].type==='array'&&properties[key].items.schema){
-                objs = []
-                for(let id of val[key]){
-                    obj = await scirichon_cache.getItemByCategoryAndID(properties[key].items.schema,id)||id
-                    objs.push(obj)
-                }
-                val[key] = objs
-            }else if(properties[key].type==='object') {
-                val[key] = await referencedObjectMapper(val[key], properties[key])
-            }else if (properties[key].type === 'array' && properties[key].items.type === 'object') {
-                for (let entry of val[key]) {
-                    entry = await referencedObjectMapper(entry, properties[key].items)
-                }
-            }
-        }
-    }
-    return val
-}
 
 const notificationObjectMapping = async function(obj) {
-    let it_services_items = [],it_services_item,article
+    let article
     if(obj){
-        if(obj.it_service&&obj.it_service.length){
-            for(let it_service_uuid of obj.it_service){
-                it_services_item = await scirichon_cache.getItemByCategoryAndID('ITService',it_service_uuid)
-                if(!_.isEmpty(it_services_item))
-                    it_services_items.push(it_services_item);
-            }
-            obj.it_service = _.isEmpty(it_services_items)?obj.it_service:it_services_items
-        }
         if(obj.article_id){
             try {
                 article = await common.apiInvoker('GET', `http://${config.get('privateIP')||'localhost'}:${config.get('kb.port')}`, `/KB/API/v1/articles/${obj.article_id}`)
@@ -77,7 +29,7 @@ const notificationObjectMapping = async function(obj) {
             }
         }
         if(obj.category) {
-            obj = await resultMapperBySchema(obj)
+            obj = await scirichon_response_mapper.referencedObjectMapper(obj)
         }
     }
     return obj
